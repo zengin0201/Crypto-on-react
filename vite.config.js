19a5a71de40393d95cd243a80d61cff1a866e871
zengin0201 <aleksejborodin711@gmail.com>
2026-03-01 20:15:19 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '127.0.0.1', // или 'localhost'
-    port: 3000 // ваш порт
-  }
+  
 })
