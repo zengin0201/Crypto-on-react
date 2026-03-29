8b04ff4e7e743c1af2fd34efb4fcbe88af061a8d
Alexey Borodin <aleksejborodin711@gmail.com>
2026-03-29 18:26:23 +0300

    Update CoinCard.jsx
    add checker error

--- src/Components/CoinCard.jsx
+++ src/Components/CoinCard.jsx
@@ -2,26 +2,30 @@ const CoinCard = ({ coin, isFav, onFavClick }) => {
   return (
     <div className="coin-card">
       <img src={coin.image} alt={coin.name} />
-      
+
       <div className="coin-info">
         <div className="coin-name">{coin.name}</div>
         <div className="coin-symbol">{coin.symbol}</div>
       </div>
 
       <div className="coin-stats">
-        <div className="coin-price">
-          ${coin.current_price.toLocaleString()}
-        </div>
-        <div className={`coin-percent ${coin.price_change_percentage_24h > 0 ? "up" : "down"}`}>
-          {coin.price_change_percentage_24h > 0 ? "+" : ""}{coin.price_change_percentage_24h.toFixed(2)}%
+        <div className="coin-price">${coin.current_price.toLocaleString()}</div>
+        <div
+          className={`coin-percent ${coin.price_change_percentage_24h > 0 ? "up" : "down"}`}
+        >
+          {coin.price_change_percentage_24h
+            ? (coin.price_change_percentage_24h > 0 ? "+" : "") +
+              coin.price_change_percentage_24h.toFixed(2) +
+              "%"
+            : "No data"}
         </div>
       </div>
 
-      <button 
-        className={`fav-btn ${isFav ? 'active' : ''}`} 
+      <button
+        className={`fav-btn ${isFav ? "active" : ""}`}
         onClick={() => onFavClick(coin.id)}
       >
-        {isFav ? '★' : '☆'}
+        {isFav ? "★" : "☆"}
       </button>
     </div>
   );
